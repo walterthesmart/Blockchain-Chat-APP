1480864fc120f24e264fb6d8782fd90fc6cccbae
walterthesmart <nwaugowalter@gmail.com>
2023-11-04 08:41:23 +0100

    learning Solidity

---
+++ constant.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+// CONSTANTS
+
+contract Constants
+{
+    
+    // 2489 gas 
+    address public MyAdd = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+}

cdb78d52b5084aa8d0ffd7842bd43ba266423be3
walterthesmart <nwaugowalter@gmail.com>
2023-11-04 11:07:53 +0100

    learning Solidity

--- constant.sol
+++ constant.sol
@@ -5,8 +5,13 @@ pragma solidity 0.8.18;
 
 contract Constants
 {
+    // 2489 gas
+    //address public myAdd = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
     
-    // 2489 gas 
-    address public MyAdd = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-
+    // 356 gas
+    address public constant MY_ADDR = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    function getConstant() public pure returns (address)
+    {
+        return MY_ADDR;
+    }
 }
